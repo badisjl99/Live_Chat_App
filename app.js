8d000b4b64b131c8e8953de74ba99ae046cd5c66
badisjl99 <badisjlassi86@gmail.com>
2024-01-20 14:24:55 +0100

    com2

--- app.js
+++ app.js
@@ -11,58 +11,61 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const randomNames = [
-  'ShadowLion', 'SilverPhoenix', 'CrimsonDragon', 'GoldenFalcon', 'EmeraldTiger',
-  'SapphireWolf', 'RubyBear', 'DiamondCheetah', 'AmberJaguar', 'ObsidianPanther',
-  'IvoryLeopard', 'OnyxGrizzly', 'TopazEagle', 'PlatinumHawk', 'BronzeLynx',
-  'JadePuma', 'AzureGiraffe', 'CobaltKangaroo', 'OpalOstrich', 'QuartzKoala'
+  const randomNames = [
+    'ShadowLion', 'SilverPhoenix', 'CrimsonDragon', 'GoldenFalcon', 'EmeraldTiger',
+    'SapphireWolf', 'RubyBear', 'DiamondCheetah', 'AmberJaguar', 'ObsidianPanther',
+    'IvoryLeopard', 'OnyxGrizzly', 'TopazEagle', 'PlatinumHawk', 'BronzeLynx',
+    'JadePuma', 'AzureGiraffe', 'CobaltKangaroo', 'OpalOstrich', 'QuartzKoala',
+    'VelvetVulture', 'SteelRaven', 'CopperFerret', 'CeruleanSparrow', 'BrassHummingbird',
+  'VermilionSwan', 'IridescentPelican', 'ZephyrPenguin', 'LunarAlbatross', 'SolarCrow',
+  'ThunderHawk', 'MysticOsprey', 'CosmicCondor', 'AbyssalSwift', 'NovaOwl',
+  'CelestialParrot', 'RogueRoc', 'EtherealRobin', 'VividPuffin', 'GildedGull'
+
+  ];
+
+  const randomColors = ['#FF5733', '#33FF57', '#5733FF', '#FF5733', '#33FF57',
+  '#5733FF', '#FF5733', '#33FF57', '#5733FF', '#FF5733', '#0099FF', '#FF66CC',
+  '#66FF66', '#FF9900', '#9966FF', '#FF3333', '#33CC33', '#FFCC00', '#6699FF', '#FF3366'
 ];
-
-const randomColors = ['#FF5733', '#33FF57', '#5733FF', '#FF5733', '#33FF57',
-  '#5733FF', '#FF5733', '#33FF57', '#5733FF', '#FF5733'];
-
-// Map to track connected users
 const connectedUsers = new Map();
 
-
+function generateUniqueColor() {
+    const existingColors = new Set(Array.from(connectedUsers.values()).map(user => user.color));
+    const availableColors = randomColors.filter(color => !existingColors.has(color));
+    return availableColors[Math.floor(Math.random() * availableColors.length)];
+}
 
 io.on('connection', (socket) => {
     console.log('A user connected');
-  
+
     const randomNameIndex = Math.floor(Math.random() * randomNames.length);
-    const randomColorIndex = Math.floor(Math.random() * randomColors.length);
-  
     const username = randomNames[randomNameIndex];
-    const color = randomColors[randomColorIndex];
-  
-    // Add the connected user to the map
+    
+    const color = generateUniqueColor();
+
     connectedUsers.set(socket.id, { username, color });
-  
-    // Emit the updated list of connected users to all clients
+
     io.emit('update connected users', Array.from(connectedUsers.values()));
-  
-    // Emit the connected event to all clients
+
     io.emit('user connected', { username, color });
-  
+
     socket.emit('assign user info', { username, color });
-  
+
     socket.on('chat message', (data) => {
-      io.emit('chat message', { ...data, sender: username, color, date: new Date().toISOString() });
+        io.emit('chat message', { ...data, sender: username, color, date: new Date().toISOString() });
     });
-  
+
     socket.on('disconnect', () => {
-      console.log('User disconnected');
-      // Remove the disconnected user from the map
-      connectedUsers.delete(socket.id);
-      // Emit the updated list of connected users to all clients
-      io.emit('update connected users', Array.from(connectedUsers.values()));
-  
-      // Emit the disconnected event to all clients
-      io.emit('user disconnected', { username, color });
+        console.log('User disconnected');
+
+        connectedUsers.delete(socket.id);
+        io.emit('update connected users', Array.from(connectedUsers.values()));
+
+        io.emit('user disconnected', { username, color });
     });
-  });
+});
   
-  // ... (existing code)
+
   
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
